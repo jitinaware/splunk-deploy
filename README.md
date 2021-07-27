ef9784f87f9ceefd2c70f43124d21d5cccc6f080
moksha-io <60826351+moksha-io@users.noreply.github.com>
2021-07-27 16:49:27 -0500

    revamped script, added license file and user credentials

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # splunk-deploy
-Collection of Splunk deployment scripts in various languages
+A collection of Splunk deployment scripts in various scenarios
 
-# To Do
-- ~~check for existing splunk user & group~~
-- find splunk installer locally first
-- check md5 hash
+# Usage
+
+1. Populate license.lic with your license file
+2. Modify user-seed.conf with the admin credentials you'd like to use
+3. Set permissions and run the script
+	1. `chmod +x [yourchosenscript].sh`
+	2. `./[yourchosenscript.sh`
+
+
--- installsplunkasrootuser.sh
+++ installsplunkasrootuser.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
 
-# Not tested on 8.x and above!
+# Tested on CentOS 7
 
 # Variables
 
 SPLUNK_HOME=/opt/splunk
-splunkuser=splunk                               # This is the user that Splunk runs as. Uncomment to enable.
-splunkgroup=splunk
-splunkuserpassword=                           # You can specify the password here, instead of the script prompting you.
-                                                # NOTE: THIS IS INSECURE. USE ONLY FOR TESTING PURPOSES!
-splunkinstallerfilename=splunk-7.3.5.tgz
-splunkinstallerdownload="wget -O $splunkinstallerfilename https://www.splunk.com/page/download_track?file=7.3.5/linux/splunk-7.3.5-86fd62efc3d7-Linux-x86_64.tgz&ac=&wget=true&name=wget&platform=Linux&architecture=x86_64&version=7.3.5&product=splunk&typed=release"
+
+# You can manually specify a password here, 
+# instead of the script prompting you 
+# (or you don't want to use the user-seed.conf method)
+# NOTE: THIS IS INSECURE. USE ONLY FOR TESTING PURPOSES!
+
+# If using user-seed.conf, uncomment this line
+#splunkuserpassword=`cat user-seed.conf | grep PASSWORD | awk '{print $3;}'`
+
+# If manually specifying a password, uncomment this
+#splunkuserpassword=Splunk.5
+
+#hostname=                                                
 
 
 
-# Read secret string
-read_secret()
-{
-    # Disable echo.
-    stty -echo
 
-    # Set up trap to ensure echo is enabled before exiting if the script
-    # is terminated while echo is disabled.
-    trap 'stty echo' EXIT
-
-    # Read secret.
-    read "$@"
-
-    # Enable echo.
-    stty echo
-    trap - EXIT
-
-    # Print a newline because the newline entered by the user after
-    # entering the passcode is not echoed. This ensures that the
-    # next line of output begins at a new line.
-    echo
-}
+# VERSION 8.2.0
+splunkpackagefilename=splunk-8.2.0.tgz
+splunkpackagedownload="wget -O $splunkpackagefilename https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.2.0&product=splunk&filename=splunk-8.2.0-e053ef3c985f-Linux-x86_64.tgz&wget=true"
 
 
 # Installs wget
@@ -45,25 +34,62 @@ fi
 
 
 
-
-## This section deals with $SPLUNK_HOME
+# This section deals with $SPLUNK_HOME
+# if it's found, try to stop splunkd, then
+# rename + move the existing Splunk install
 
 
 if [ -x $SPLUNK_HOME ] ; then
     echo "$SPLUNK_HOME found, renaming and moving to /tmp/";
+    sudo $SPLUNK_HOME/bin/splunk stop
     sudo mv -v $SPLUNK_HOME /tmp/splunk_$(date +%d-%m-%Y_%H:%M:%S)
 else
     echo "$SPLUNK_HOME not found, progressing with installation..."
 fi
 
 sudo mkdir $SPLUNK_HOME
-$splunkinstallerdownload
-sudo tar -xzvC /opt -f $splunkinstallerfilename
-sudo chown -vR root $SPLUNK_HOME                                                             # Takes ownership of $SPLUNK_HOME to $splunkuser
 
-$SPLUNK_HOME/bin/splunk start --accept-license                                               # Starts splunk as $splunkuser, still needs password input, admin acct not created
-sudo $SPLUNK_HOME/bin/splunk enable boot-start                                               # Sets splunk to start on boot  
+# This section deals with $splunkpackagefilename
+# if it's found, proceed with installation
+# if it's not found, download it
 
-#$SPLUNK_HOME/bin/splunk set default-hostname $hostname
-#$SPLUNK_HOME/bin/splunk set servername $hostname
-#$SPLUNK_HOME/bin/splunk edit licenser-localslave -master_uri 'https://master:port'      # Add license slave
+if [ -a ./$splunkpackagefilename ] ; then
+    echo "$splunkpackagefilename found, progressing with installation";
+else
+    echo "$splunkpackagefilename not found, downloading..."
+    $splunkpackagedownload
+fi
+
+
+# Untar the package
+sudo tar -xzvC /opt -f $splunkpackagefilename
+
+# Takes ownership of $SPLUNK_HOME to root
+sudo chown -vR root $SPLUNK_HOME                                                             
+
+
+# Starts splunk as $splunkuser, still needs password input, admin acct not created
+# required if using the $splunkuserpassword variable above
+#sudo $SPLUNK_HOME/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd $splunkuserpassword                                               
+
+# Starts splunk as root, auto generates password
+sudo $SPLUNK_HOME/bin/splunk enable boot-start --accept-license --answer-yes --no-prompt
+
+# Copies user-seed.conf so our preferred password is set
+sudo cp ./user-seed.conf $SPLUNK_HOME/etc/system/local/user-seed.conf
+
+# Add license from local license file
+sudo $SPLUNK_HOME/bin/splunk add licenses ./license.lic -auth admin:$splunkuserpassword
+
+# Set default hostname and servername
+sudo $SPLUNK_HOME/bin/splunk set default-hostname $hostname
+sudo $SPLUNK_HOME/bin/splunk set servername $hostname
+
+sudo $SPLUNK_HOME/bin/splunk restart
+
+# Cleanup items, uncomment to enable
+echo "Performing cleanup actions..."
+unset splunkuserpassword
+#sudo rm -f ./user-seed.conf
+sudo rm -f ./$splunkpackagefilename
+#sudo rm -f ./license.lic
